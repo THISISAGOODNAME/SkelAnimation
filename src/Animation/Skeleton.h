15e1a6451ef7ce1a10a96854d047e73545456cec
THISISAGOODNAME <yangyj19931231@gmail.com>
2021-07-22 00:51:56 +0800

    Skeleton

---
+++ src/Animation/Skeleton.h
@@ -0,0 +1,33 @@
+#ifndef _H_SKELETON_
+#define _H_SKELETON_
+
+#include "Animation/Pose.h"
+#include "Math/mat4.h"
+#include <vector>
+#include <string>
+
+class Skeleton {
+protected:
+    Pose mRestPose;
+    Pose mBindPose;
+    std::vector<mat4> mInvBindPose;
+    std::vector<std::string> mJointNames;
+
+protected:
+    void UpdateInverseBindPose();
+
+public:
+    Skeleton();
+    Skeleton(const Pose& rest, const Pose& bind, const std::vector<std::string>& names);
+
+    void Set(const Pose& rest, const Pose& bind, const std::vector<std::string>& names);
+
+    Pose& GetBindPose();
+    Pose& GetRestPose();
+    std::vector<mat4>& GetInvBindPose();
+
+    std::vector<std::string>& GetJointNames();
+    std::string& GetJointName(unsigned int index);
+};
+
+#endif
